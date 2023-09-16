e14a8b7db8f465fc9626cb0c948be319dc197c6a
Ilias Benharrat <benbeckerben4@gmail.com>
2023-09-16 18:17:49 +0200

    L'utilisateur peut mettre son nom et son code pin (après 3 essais, il est bloqué)

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+def AccountConnexion():
+    name = "BENHARRAT"
+    pinCode = "1207"
+    connectionAttempt = 0
+
+    while connectionAttempt < 3:
+        userName = input("Entrer votre nom d'utilisateur : ")
+        writePinCode = input("Entrer votre Code Pin : ")
+
+        if userName == name and writePinCode == pinCode:
+            print("Votre authentification est valide ! Bonjour " + name + " !")
+            break
+        else:
+            print("Votre nom d'authentification ou bien votre mot de passe est incorrect.")
+            connectionAttempt += 1
+
+    if connectionAttempt == 3:
+        print("Compte bloqué ! Trop de tentatives de connexion ont été faites, contacter votre conseiller bancaire.")
+
+AccountConnexion()
