fa94a0dd18fc029de818859e52de1350301323ce
Ilias Benharrat <benbeckerben4@gmail.com>
2023-09-17 09:40:05 +0200

    Ajout de la possibilité de consulter son solde en cas de bonne authentification

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-def AccountConnexion():
-    name = "BENHARRAT"
-    pinCode = "1207"
-    connectionAttempt = 0
+import random
+name = "BENHARRAT"
+pinCode = "1207"
+connectionAttempt = 0
+accountBalance = random.uniform(1.0, 10000.0)
+accountBalanceRounded = round(accountBalance, 2)
 
-    while connectionAttempt < 3:
-        userName = input("Entrer votre nom d'utilisateur : ")
-        writePinCode = input("Entrer votre Code Pin : ")
+while connectionAttempt < 3:
+    userName = input("Entrer votre nom d'utilisateur : ")
+    writePinCode = input("Entrer votre Code Pin : ")
 
-        if userName == name and writePinCode == pinCode:
-            print("Votre authentification est valide ! Bonjour " + name + " !")
-            break
-        else:
-            print("Votre nom d'authentification ou bien votre mot de passe est incorrect.")
-            connectionAttempt += 1
+    if userName == name and writePinCode == pinCode:
+        print("Votre authentification est valide ! Bonjour " + name + " !\nVotre solde est de " + str(accountBalanceRounded) + " €.")
+        break
+    else:
+        print("Votre nom d'authentification ou bien votre mot de passe est incorrect.")
+        connectionAttempt += 1
 
     if connectionAttempt == 3:
-        print("Compte bloqué ! Trop de tentatives de connexion ont été faites, contacter votre conseiller bancaire.")
-
-AccountConnexion()
+        print("Compte bloqué ! Trop de tentatives de connexion ont été faites. Contacter votre conseiller bancaire.")
