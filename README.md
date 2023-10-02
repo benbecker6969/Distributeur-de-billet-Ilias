5b05a5295da19f9ff40c6b16a785cb02153cc81b
Ilias Benharrat <benbeckerben4@gmail.com>
2023-10-02 21:47:42 +0200

    Correction de toutes les erreurs, ajout de commentaire, mise en page des print et des input, gestion des erreurs côté utilisateurs, Création du readme.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-#Ceci est un test.
+# Gestionnaire de Compte Bancaire
+
+Ce gestionnaire de compte bancaire est un programme Python simple conçu pour permettre aux utilisateurs de gérer leurs comptes, effectuer des retraits et consulter leur historique de transactions. Le code est prévu pour être utilisé en ligne de commande.
+
+## Utilisation
+
+1. Au démarrage, le programme invite l'utilisateur à s'authentifier en entrant son nom d'utilisateur et son code PIN. Par défaut, le nom d'utilisateur est "BENHARRAT" et le code PIN est "1207". L'utilisateur dispose de trois tentatives pour s'authentifier correctement.
+
+2. Une fois l'authentification réussie, l'utilisateur peut choisir parmi plusieurs options :
+
+   - Pour effectuer un retrait (dans la limite de 200€ par jour), l'option "1" permet à l'utilisateur de spécifier le montant à retirer ainsi que le choix entre le moins de billets possible (option "1") ou le plus de billets possible (option "2").
+
+   - L'option "2" permet d'afficher un reçu contenant les détails des 5 dernières transactions. Si aucune transaction n'a été effectuée, un message indique que l'historique est vide.
+
+   - Enfin, l'option "3" permet de quitter le programme.
+
+3. Lorsqu'un utilisateur souhaite effectuer un retrait, le programme vérifie d'abord si le montant total retiré au cours de la journée n'excède pas 200€. Si c'est le cas, l'utilisateur est invité à indiquer le montant souhaité et à choisir entre le moins de billets possible ou le plus de billets possible. Le montant est ensuite déduit du solde du compte.
+
+4. L'historique des transactions est conservé, et les 5 dernières transactions sont affichées si l'utilisateur le souhaite.
+
+5. Notez que l'utilisateur est autorisé à effectuer un maximum de 5 retraits par jour. Une fois ce nombre atteint, il ne pourra plus effectuer de retraits jusqu'au jour suivant.
+
+## Gestion des Erreurs
+
+Ce programme a été amélioré pour gérer les erreurs d'entrée utilisateur de manière plus robuste. Par exemple :
+
+- Si l'utilisateur entre un montant de retrait invalide (supérieur à 200€), le programme le guidera pour entrer une valeur valide.
+
+- Si l'utilisateur choisit une option invalide, il sera invité à réessayer jusqu'à ce qu'une option valide soit choisie.
+
+- Si l'utilisateur entre un nom d'utilisateur ou un code PIN incorrect à trois reprises, le compte sera bloqué, empêchant ainsi toute connexion ultérieure.
+
+---
+
+C'est ainsi que fonctionne ce gestionnaire de compte bancaire simple en Python. Il peut être utilisé pour simuler des opérations bancaires de base.
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import random
 
+# Variables
 name = "BENHARRAT"
 pinCode = "1207"
 connectionAttempt = 0
 accountBalance = random.uniform(1.0, 10000.0)
-accountBalanceRounded = round(accountBalance, 2)
+accountBalanceRounded: float = round(accountBalance, 2)
 historyOfTheTransaction = []
+accountBlocked = False
+numberOfWithdrawals = 0
 
+# Authentification de l'utilisateur
 while connectionAttempt < 3:
     userName = input("Entrer votre nom d'utilisateur : ")
     writePinCode = input("Entrer votre Code Pin : ")
@@ -21,33 +25,49 @@ while connectionAttempt < 3:
 
     if connectionAttempt == 3:
         print("Compte bloqué ! Trop de tentatives de connexion ont été faites. Contacter votre conseiller bancaire.")
-
-# Utilisateur peut retirer un maximum de EUR 200 par jour ou le maximum de son solde si c'est moins que 200 EUR :
-# L'utilisateur a le choix de pouvoir selectionner ce qui veut retirer en therme de billet à la ligne 36
-stockNumberToWithdraw = 0.0
-while True:
-    choiceOfUser = input("Pour retirer, tapper 1:\nPour générer un reçu avec vos 5 dernières transactions, "
-                         "tapper 2:\nPour partir, tapper 3: ")
-    if choiceOfUser == "1":
-        numberToWithdraw = float(input("Rentrer le montant que vous souhaiter retirer : "))
-        stockNumberToWithdraw += numberToWithdraw
-        if stockNumberToWithdraw > 200:
-            print("Impossible de retier plus de 200€ par jour !")
-        else:
-            quantityOfBill = int(input("Tapper 1 si vous souhaitez avoir le moins de billet possible:\n"
-                                       "Tapper 2 si vous souhaitez avoir le plus de billet possible:"))
-            accountBalanceRounded = accountBalanceRounded - numberToWithdraw
-            print(
-                "Vous venez de retier " + str(numberToWithdraw) + "€ avec succès.\n Voici votre nouveau solde : " + str(
-                    accountBalanceRounded))
-            historyOfTheTransaction.append(numberToWithdraw)
-    elif choiceOfUser == "2" and stockNumberToWithdraw != 0:
-        print("Voici vos dernières opération.\nIl faut savoir que le maximum de l'historique est de 5 opération: ")
-        for i, transaction in enumerate(historyOfTheTransaction[-5:]):
-            i = i + 1
-            print("Transaction " + str(i) + " : " + str(transaction) + " €")
-    elif choiceOfUser == "2" and stockNumberToWithdraw == 0:
-        print("Vous ne possédez aucune transaction. Il est donc impossible de sortir un historique.")
-    elif choiceOfUser == "3":
-        print("Merci beacoup pour votre fidélité, XEFIBank vous souhaite une bonne journée !")
+        accountBlocked = True
         break
+#Retraits + historique
+if not accountBlocked:
+    stockNumberToWithdraw = 0.0
+
+    while True:
+        if numberOfWithdrawals > 5:
+            print("Vous ne pouvez retirer plus de 5 fois par jour.")
+            break
+
+        choiceOfUser = input("Pour retirer, tapper 1 :\nPour générer un reçu avec vos 5 dernières transactions, "
+                             "tapper 2 :\nPour partir, tapper 3 : ")
+        if choiceOfUser == "1":
+            numberToWithdraw = float(input("Rentrer le montant que vous souhaiter retirer : "))
+            stockNumberToWithdraw += numberToWithdraw
+            if stockNumberToWithdraw > 200:
+                print("Impossible de retirer plus de 200€ par jour !")
+                stockNumberToWithdraw = 0.0
+            elif stockNumberToWithdraw < 200:
+                quantityOfBill = int(input("Tapper 1 si vous souhaitez avoir le moins de billet possible : \n"
+                                           "Tapper 2 si vous souhaitez avoir le plus de billet possible : "))
+                if quantityOfBill == 1 or quantityOfBill == 2:
+                    accountBalanceRounded = accountBalanceRounded - numberToWithdraw
+                    print(
+                        "Vous venez de retirer " + str(numberToWithdraw) + "€ avec succès.\nVoici votre nouveau solde "
+                                                                           ": " +
+                        str(accountBalanceRounded) + "€")
+                    historyOfTheTransaction.append(numberToWithdraw)
+                    numberOfWithdrawals += 1
+                else:
+                    print("S'il vous plaît, entrer une option comprise entre 1 et 2. ")
+
+        elif choiceOfUser == "2" and stockNumberToWithdraw != 0:
+            print("Voici vos dernières opération.\nIl faut savoir que le maximum de l'historique est de 5 opérations : "
+                  )
+            for i, transaction in enumerate(historyOfTheTransaction[-5:]):
+                i = i + 1
+                print("Transaction " + str(i) + " : " + str(transaction) + "€")
+        elif choiceOfUser == "2" and stockNumberToWithdraw == 0:
+            print("Vous ne possédez aucune transaction. Il est donc impossible de sortir un historique.")
+        elif choiceOfUser == "3":
+            print("Merci beaucoup pour votre fidélité, XEFIBank vous souhaite une bonne journée !")
+            break
+        elif choiceOfUser != "1" or choiceOfUser != "2" or choiceOfUser != "3":
+            print("S'il vous plaît, entrer une option comprise entre 1 et 3. ")
